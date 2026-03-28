f7a854e3e9225d6679049549baedd7d1cbeacc8f
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-28 15:13:53 +0000

    abs

--- Abstraction.java
+++ Abstraction.java
@@ -1,16 +1,45 @@
-abstract class Shape {
-    abstract void draw();
+// abstract class Shape {
+//     abstract void draw();
+// }
+
+// class Circle extends Shape {
+//     void draw() {
+//         System.out.println("Drawing Circle");
+//     }
+// }
+
+// public class Abstraction {
+//     public static void main(String[] args) {
+//         Shape s = new Circle();
+//         s.draw();  // Output: Drawing Circle
+//     }
+// }
+
+
+
+//exampleeeeeeeeeeeeeeeeeeeeeeeee 
+abstract class Vehicle {
+    abstract void start(); // abstract method
 }
 
-class Circle extends Shape {
-    void draw() {
-        System.out.println("Drawing Circle");
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car starts with a key");
     }
 }
 
-public class Abstraction {
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("Bike starts with a kick");
+    }
+}
+
+public class Abstraction{
     public static void main(String[] args) {
-        Shape s = new Circle();
-        s.draw();  // Output: Drawing Circle
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+        v1.start(); // Output: Car starts with a key
+        v2.start(); // Output: Bike starts with a kick
     }
 }
+
