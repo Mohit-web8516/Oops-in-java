da514e03bd79444ee0117333aeac8e5a9531d7e7
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-28 15:16:47 +0000

    inheritence

--- inheritence.java
+++ inheritence.java
@@ -1,18 +1,45 @@
-class Animal {
-    void sound() {
-        System.out.println("Animal makes a sound");
+// class Animal {
+//     void sound() {
+//         System.out.println("Animal makes a sound");
+//     }
+// }
+
+// class Dog extends Animal {
+//     void sound() {
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// public class inheritence {
+//     public static void main(String[] args) {
+//         Dog d = new Dog();
+//         d.sound();  // Output: Dog barks
+//     }
+// }
+
+
+
+//EXAMPLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+
+class Employee {
+    String name;
+    void work() {
+        System.out.println(name + " is working");
     }
 }
 
-class Dog extends Animal {
-    void sound() {
-        System.out.println("Dog barks");
+class Manager extends Employee {
+    void manageTeam() {
+        System.out.println(name + " is managing the team");
     }
 }
 
 public class inheritence {
     public static void main(String[] args) {
-        Dog d = new Dog();
-        d.sound();  // Output: Dog barks
+        Manager m = new Manager();
+        m.name = "ALEX";
+        m.work();        // Inherited method
+        m.manageTeam();  // Manager-specific method
     }
 }
