5d9f84103cf02e13f0dfd76cd0f1cbd450db6d3b
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-28 15:17:33 +0000

    polymorphism

--- polymorphism.java
+++ polymorphism.java
@@ -22,21 +22,51 @@
 
 //Method Overriding Examples:
 
-class Vehicle {
-    void run() {
-        System.out.println("Vehicle is running");
+// class Vehicle {
+//     void run() {
+//         System.out.println("Vehicle is running");
+//     }
+// }
+
+// class Bike extends Vehicle {
+//     void run() {
+//         System.out.println("Bike is running safely");
+//     }
+// }
+
+// public class polymorphism {
+//     public static void main(String[] args) {
+//         Vehicle v = new Bike();  // Runtime polymorphism
+//         v.run();  // Output: Bike is running safely
+//     }
+// }
+
+
+//EXAMPLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+class Shape {
+    void draw() {
+        System.out.println("Drawing a shape");
     }
 }
 
-class Bike extends Vehicle {
-    void run() {
-        System.out.println("Bike is running safely");
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing a rectangle");
     }
 }
 
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+// Public class name must match filename
 public class polymorphism {
     public static void main(String[] args) {
-        Vehicle v = new Bike();  // Runtime polymorphism
-        v.run();  // Output: Bike is running safely
+        Shape s1 = new Rectangle(); // runtime polymorphism
+        Shape s2 = new Circle();
+        s1.draw(); // Output: Drawing a rectangle
+        s2.draw(); // Output: Drawing a circle
     }
 }
