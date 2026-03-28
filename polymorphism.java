705c00f8bd7d0d6a2353598dc4d69ee4b7ecce46
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-28 14:32:05 +0000

    polymorphism

--- polymorphism.java
+++ polymorphism.java
@@ -1,19 +1,42 @@
 
 //Method Overloading Example:
 
-class MathUtil {
-    int add(int a, int b) {
-        return a + b;
-    }
-    double add(double a, double b) {
-        return a + b;
+// class MathUtil {
+//     int add(int a, int b) {
+//         return a + b;
+//     }
+//     double add(double a, double b) {
+//         return a + b;
+//     }
+// }
+
+// public class polymorphism{
+//     public static void main(String[] args) {
+//         MathUtil m = new MathUtil();
+//         System.out.println(m.add(5, 10));       // 15
+//         System.out.println(m.add(5.5, 4.5));    // 10.0
+//     }
+// }
+
+
+
+//Method Overriding Examples:
+
+class Vehicle {
+    void run() {
+        System.out.println("Vehicle is running");
     }
 }
 
-public class polymorphism{
+class Bike extends Vehicle {
+    void run() {
+        System.out.println("Bike is running safely");
+    }
+}
+
+public class polymorphism {
     public static void main(String[] args) {
-        MathUtil m = new MathUtil();
-        System.out.println(m.add(5, 10));       // 15
-        System.out.println(m.add(5.5, 4.5));    // 10.0
+        Vehicle v = new Bike();  // Runtime polymorphism
+        v.run();  // Output: Bike is running safely
     }
 }
