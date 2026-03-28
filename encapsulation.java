f9ad8c09798e197d27e927f384201c18f5fde603
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-28 15:16:12 +0000

    encap

--- encapsulation.java
+++ encapsulation.java
@@ -1,19 +1,47 @@
-class Student {
-    private String name;
+// class Student {
+//     private String name;
 
-    public void setName(String name) {
-        this.name = name;  // Setter
-    }
+//     public void setName(String name) {
+//         this.name = name;  // Setter
+//     }
 
-    public String getName() {
-        return name;       // Getter
-    }
-}
+//     public String getName() {
+//         return name;       // Getter
+//     }
+// }
 
-public class encapsulation {
-    public static void main(String[] args) {
-        Student s = new Student();
-        s.setName("john");
-        System.out.println("Student name: " + s.getName());
-    }
-}
+// public class encapsulation {
+//     public static void main(String[] args) {
+//         Student s = new Student();
+//         s.setName("john");
+//         System.out.println("Student name: " + s.getName());
+//     }
+// }
+
+
+//EXAMPLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+
+// class BankAccount {
+//     private double balance; // private field
+
+//     // Setter
+//     public void deposit(double amount) {
+//         if (amount > 0) {
+//             balance += amount;
+//         }
+//     }
+
+//     // Getter
+//     public double getBalance() {
+//         return balance;
+//     }
+// }
+
+// public class encapsulation {
+//     public static void main(String[] args) {
+//         BankAccount acc = new BankAccount();
+//         acc.deposit(5000); // controlled access
+//         System.out.println("Balance: " + acc.getBalance()); // Output: Balance: 5000.0
+//     }
+// }
