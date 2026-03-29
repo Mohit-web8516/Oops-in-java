c88e22cbc2415dd01d424b93a4d0f6ffabe201a1
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-29 11:51:52 +0000

    Loop

---
+++ Java-Basics/loop.java
@@ -0,0 +1,54 @@
+//fOR-LOOP
+
+// public class loop {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             System.out.println("Ye Raha : " + i);
+//         }
+//     }
+// }
+
+
+///EXAMPLE
+
+// public class loop{
+//     public static void main(String[] args){
+//         int b =22;
+//         for(int i = 1 ; i <= b;i++){
+//             System.out.println("ye lo :" + i);
+//         }
+//     }
+// }
+
+
+/////////////////////////////////////////////////////
+/// 
+/// 
+/// 
+/// 
+/// while loop
+// public class loop {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while (i <= 5) {
+//             System.out.println("Iteration: " + i);
+//             i=i+1;
+//         }
+//     }
+// }
+ 
+
+
+///////////////////////////////////////////////////////////
+/// 
+/// DO-WHILE LOOP
+public class loop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("furrrrrrr: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
+ 
