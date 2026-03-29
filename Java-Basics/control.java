c5a7a6f0fa7d268489637fa1f0fda8db6c69a822
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-29 11:51:27 +0000

    control

---
+++ Java-Basics/control.java
@@ -0,0 +1,31 @@
+//Break statement
+
+// public class control {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             if (i == 3) {
+//                 break; // exits loop when i=3
+//             }
+//             System.out.println("i = " + i);
+//         }
+//     }
+// }
+
+
+
+
+//////////////////////////////////////////////////
+/// ///////////////////////////////////////////
+//Continue Statement
+
+
+// public class control {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             if (i == 3) {
+//                 continue; // skips printing when i=3
+//             }
+//             System.out.println("i = " + i);
+//         }
+//     }
+// }
