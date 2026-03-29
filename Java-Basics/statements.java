5b1c9f0adad34d3a857093d81428cc04b8b7fc1d
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-29 11:24:46 +0000

    statements

---
+++ Java-Basics/statements.java
@@ -0,0 +1,19 @@
+//IF-Statement
+// public class statements{
+//     public static void main(String[] args){
+//         int a = 10;
+//         if(a > 9){
+//             System.out.println("you are right!!!!!");
+//         }
+//     }
+// }
+/////////////////////////////////////////////////
+
+// public class statements {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         if (a > 5) {
+//             System.out.println("correct");
+//         }
+//     }
+// }
