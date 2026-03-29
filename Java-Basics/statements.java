dc095ced2d058abd0392dcde3fd4af5777cfc7bc
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-29 11:52:29 +0000

    Statement

--- Java-Basics/statements.java
+++ Java-Basics/statements.java
@@ -17,3 +17,57 @@
 //         }
 //     }
 // }
+
+
+
+////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////
+//IF-ELSE STATEMENTS
+
+
+
+// public class statements{
+//     public static void main(String[] args){
+//         int b = 4;
+//         if(b > 2){
+//             System.out.println("On THE RIGHT WAY ");
+//             } else {
+//              System.out.println("wrong");    
+//             }      
+//     }
+// }
+
+////////////////////////////////////////////////////
+// public class statements {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         if (a < 5) {
+//             System.out.println("wrong");
+//         } else {
+//             System.out.println("right");
+//         }
+//     }
+// }
+
+
+
+
+///////////////////////////////////////////////////////////////////
+/// 
+//IF-ELse if -ELSE STATEMENT
+
+
+// public class ElseIfDemo {
+//     public static void main(String[] args) {
+//         int marks = 82;
+//         if (marks >= 90) {
+//             System.out.println("Grade: A");
+//         } else if (marks >= 75) {
+//             System.out.println("Grade: B");
+//         } else if (marks >= 60) {
+//             System.out.println("Grade: C");
+//         } else {
+//             System.out.println("Grade: D");
+//         }
+//     }
+// }
